6da8f7d838673f5099a506eb627a626597465d42
adam <afhodges@pm.me>
2026-06-25 13:06:04 +0200

    refactor package name

---
+++ app/src/test/java/dev/auroralaboratories/trailweight/WeightFormatTest.kt
@@ -0,0 +1,19 @@
+//package dev.auroralaboratories.trailweight
+//
+//import dev.auroralaboratories.trailweight.preferences.formatWeight
+//import dev.auroralaboratories.trailweight.preferences.UnitPreferences
+//import org.junit.Test
+//import org.junit.Assert.assertEquals
+//
+//
+//class WeightFormatTest{
+//
+//    @Test
+//    fun `grams to oz conversion is correct`(){
+//        val weightInGrams = 1.0
+//        val result = weightFormat(weightInGrams)
+//        assetEquals(23.34952)
+//    }
+//
+//}
+//
